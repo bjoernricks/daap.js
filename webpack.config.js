c9128ff02fed48fbe42fc9a8945339b7a65bb026
Björn Ricks <bjoern.ricks@gmail.com>
2016-10-13 20:45:20 +0200

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+/*
+/* (c) 2016 Björn Ricks <bjoern.ricks@gmail.com>
+/*
+/* See LICENSE comming with the source of daap.js for details.
+*/
+
+module.exports = {
+    entry: './src/daap.js',
+    output: {
+        path: './build',
+        pathinfo: true,
+        filename: 'bundle.js',
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loader: 'eslint',
+            }
+        ],
+        loaders: [
+            {
+                test: /\.jss$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
