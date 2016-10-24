bac04ff94133d3c517f51fb2a97d1c6da2440aab
Björn Ricks <bjoern.ricks@gmail.com>
2016-10-24 19:27:45 +0200

    Generalize Items class to support all Item classes

--- src/items.js
+++ src/items.js
@@ -5,33 +5,32 @@
 */
 
 import {is_defined} from './utils.js';
-
-import Song from './song.js';
+import Item from './item.js';
 
 export class Items {
 
-    constructor(data, db_id, session_id, url) {
+    constructor(data, db_id, session_id, url, clazz = Item) {
         this._data = data;
         this._db_id = db_id;
         this._session_id = session_id;
         this._url = url;
         let items = data.find('mlcl');
-        this._song = items.find('mlit');
-        this._length = data.get('mrco');
+        this._item = items.find('mlit');
+        this._clazz = clazz;
     }
 
     get length() {
-        return this._length;
+        return this._data.get('mrco');
     }
 
     get(max) {
         let results = [];
 
-        for (let i = 0; is_defined(this._song) && this._song.isValid() &&
+        for (let i = 0; is_defined(this._item) && this._item.isValid() &&
                 (!is_defined(max) || i < max); i++) {
-            results.push(new Song(this._song, this._db_id, this._session_id,
-                this._url));
-            this._song = this._song.next();
+            results.push(new this.clazz(this._item, this._db_id,
+                this._session_id, this._url));
+            this._item = this._item.next();
         }
 
         return results;
@@ -42,13 +41,13 @@ export class Items {
     }
 
     next() {
-        let song = this._song;
-        let has_song = is_defined(song) && song.isValid();
-        this._song = this._song.next();
+        let item = this._item;
+        let has_item = is_defined(item) && item.isValid();
+        this._item = this._item.next();
         return {
-            done: !has_song,
-            value: has_song ?
-            new Song(song, this._db_id, this._session_id, this._url) :
+            done: !has_item,
+            value: has_item ?
+            new this.clazz(item, this._db_id, this._session_id, this._url) :
             undefined,
         };
     }
